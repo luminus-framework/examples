5ff6c20d51b2124b6621668e8c8a9b7aff260217
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2017-10-14 13:04:57 -0400

    Update core.clj

--- guestbook-cljs/test/clj/guestbook/test/db/core.clj
+++ guestbook-cljs/test/clj/guestbook/test/db/core.clj
@@ -23,22 +23,20 @@
     (migrations/migrate ["migrate"] (env :database-url))
     (f)))
 
-(deftest test-users
+(deftest test-message
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+    (let [timestamp (org.joda.time.DateTime. org.joda.time.DateTimeZone/UTC)]
+      (is (= 1 (db/save-message!
+                t-conn
+                {:name "Bob"
+                 :message "Hello, World"
+                 :timestamp timestamp}
+                {:connection t-conn})))
+      (is (=
+           {:name "Bob"
+            :message "Hello, World"
+            :timestamp timestamp}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message :timestamp])))))))
